12408884c8ae1a3abaaeadc13854ce472c057554
andreikatata <andreevOandrei@gmail.com>
2020-09-04 20:19:50 +0100

    pixel art, player movement, enemy movement, combat system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float xSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float checkRadius;
+    [SerializeField] int extraJumpsValue;
+
+    Rigidbody2D rb;
+
+    public Transform groundCheck;
+    private bool isGrounded;
+    [SerializeField] LayerMask WhatIsGround;
+
+    private bool facingRight = true;
+    private int extraJumps;
+    private float moveInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, WhatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * xSpeed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        } else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+    void Update()
+    {
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip() // fliping the character sides
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+   
+}
